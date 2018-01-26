f22ade30ce9ab915a6bd592a8cec5baa56e80966
Meghna <meghnabasu05@gmail.com>
2018-01-26 09:16:47 -0600

    Testing

--- GameOfLife.asm
+++ GameOfLife.asm
@@ -13,7 +13,7 @@
 #	2	Any live cell with two or three live neighbors lives on to the next generation.			#
 #	3	Any live cell with more than three live neighbors dies, as if by overpopulation.		#
 #	4	Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.	#
-#	The initial pattern constitutes the seed of the system. The rules continue to be applied repeatedly 	#
+#The initial pattern constitutes the seed of the system. The rules continue to be applied repeatedly     	#
 #														#
 #################################################################################################################
 
